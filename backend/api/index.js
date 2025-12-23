ec9bb0ffa19dabb34af8f3980264d6b00ddc9332
walisson27 <walisson_souza7@hotmail.com>
2025-12-23 13:14:19 -0300

    backend update

--- backend/api/index.js
+++
@@ -1,3 +0,0 @@
-import app from "../index.ts"
-
-export default app
--- backend/index.ts
+++ backend/index.ts
@@ -97,5 +97,3 @@ app.use((_req, res) => {
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
-
-export default app
